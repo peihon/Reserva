e7d05d2e9ef9d436789336fd67cf801e8fcbcf5e
roberto luiz <robertoluizklein@gmail.com>
2021-04-13 15:55:47 -0300

    Initial commit

---
+++ src/main/java/com/uniamerica/prova2/controller/CarroController.java
@@ -0,0 +1,56 @@
+package com.uniamerica.prova2.controller;
+
+
+import com.uniamerica.prova2.model.Carro;
+import com.uniamerica.prova2.model.Marca;
+import com.uniamerica.prova2.service.CarroService;
+import com.uniamerica.prova2.service.MarcaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequestMapping("/carros")
+public class CarroController {
+
+    private final CarroService carroService;
+
+    private final MarcaService marcaService;
+
+
+    @Autowired
+    public CarroController(CarroService carroService, MarcaService marcaService) {
+        this.carroService = carroService;
+        this.marcaService = marcaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insertCarro(@RequestBody Carro carro) throws Exception {
+        Carro add;
+        try {
+            add = carroService.insereCarro(carro);
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+        return new ResponseEntity<>(add, null, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/marca/{id}")
+    public ResponseEntity getChannelsByRecloser(@PathVariable long id) {
+
+        Marca marca = marcaService.getById(id);
+
+        List<Carro> list = carroService.getCarrosByMarca(marca);
+
+        if (!list.isEmpty()) {
+            return new ResponseEntity<>(list, null, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(list, null, HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/main/java/com/uniamerica/prova2/model/Carro.java
@@ -0,0 +1,40 @@
+package com.uniamerica.prova2.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String placa;
+
+    @ManyToOne
+    private Modelo modelo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+}
